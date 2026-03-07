eb4abe265ca11968f1ac54fae472822985495918
Devesh326 <agarwaldevesh326@gmail.com>
2026-03-07 19:19:55 +0530

    added githubController file

--- backend/src/controllers/githubController.ts
+++ backend/src/controllers/githubController.ts
@@ -1,15 +1,14 @@
-import { Octokit } from "@octokit/core";
 import { getFolderStructure } from "../services/fileSelector.js";
 import { analyzeRepo } from "../services/analyzer.js";
+import {docQueue} from "../queues/docQueue.js";
+import { Octokit } from "@octokit/core";
 
-const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN,
-});
 
 
 let repoTree: any = null;
 
 const githubRepoGet = async (_req: any, res: any) => {
+  const octokit = new Octokit({})
   try {
     const response = await octokit.request(
       "GET /repos/Devesh326/contest-list-scrapper/git/trees/main?recursive=1",
@@ -31,13 +30,15 @@ const githubRepoGet = async (_req: any, res: any) => {
     res.status(500).json({ error: "GitHub API failed" });
   }
 };
-
-const githubRepoTopLevelGet = async (_req: any, res: any) => {
+// const githubRepoTopLevelGet = async (_req: any, res: any) => {
+const githubRepoTopLevelGet = async (octokit: any, owner: String, repoName: String, branchRef: String) => {
     try {
-    const owner = 'Devesh326', repo = 'contest-list-scrapper', branch = 'main' 
+    const repo = repoName, branch = branchRef 
     
     if (!owner || !repo) {
-      return res.status(400).json({ error: 'owner and repo are required' });
+      // return res.status(400).json({ error: 'owner and repo are required' });
+      console.log('owner and repo are required');
+      return;      
     }
     
     // Get branch SHA
@@ -51,12 +52,12 @@ const githubRepoTopLevelGet = async (_req: any, res: any) => {
     
     // Get tree
     const treeResponse = await octokit.request(
-      "GET /repos/Devesh326/contest-list-scrapper/git/trees/main?recursive=1",
+      `GET /repos/${owner}/${repo}/git/trees/${branch}?recursive=1`,
       {
-        owner: "Devesh326",
-        repo: "contest-list-scrapper",
+        owner,
+        repo: repoName,
         tree_sha: 'sha-1',
-        truncated: true,
+        refs: branchRef,
         headers: {
           "X-GitHub-Api-Version": "2022-11-28",
         },
@@ -70,9 +71,9 @@ const githubRepoTopLevelGet = async (_req: any, res: any) => {
         console.log("inside the fetch file helper function");
         
       try {
-        const data = await octokit.request(`GET /repos/Devesh326/contest-list-scrapper/git/blobs/${pkg.sha}`, {
-            owner: 'Devesh326',
-            repo: 'contest-list-scrapper',
+        const data = await octokit.request(`GET /repos/${owner}/${repoName}/git/blobs/${pkg.sha}`, {
+            owner,
+            repo: repoName,
             path:'backend',
             sha: pkg.sha,
             headers: {
@@ -82,7 +83,7 @@ const githubRepoTopLevelGet = async (_req: any, res: any) => {
         
         console.log("Successfully fetched file:", pkg.path);
         if (data.data.content) {
-            console.log(Buffer.from(data.data.content as string, 'base64').toString('utf-8'));
+            // console.log(Buffer.from(data.data.content as string, 'base64').toString('utf-8'));
             
             
           return Buffer.from(data.data.content as string, 'base64').toString('utf-8');
@@ -101,27 +102,51 @@ const githubRepoTopLevelGet = async (_req: any, res: any) => {
       fetchFile
     );
     
-    res.json({
-      success: true,
-      analysis
-    });
+    // res.json({
+    //   success: true,
+    //   analysis
+    // });
+    // console.log("========= ANALYSIS =========", analysis);
+    let val = JSON.stringify(analysis);
+    console.log("========= ANALYSIS STRINGIFIED =========", val);
+
+    return analysis;
     
   } catch (error: any) {
     console.error('Analysis error:', error);
-    res.status(500).json({ 
-      error: 'Failed to analyze repository',
-      message: error.message 
-    });
+    // res.status(500).json({ 
+    //   error: 'Failed to analyze repository',
+    //   message: error.message 
+    // });
   }
 }
 
-const repoPathGet = async (req, res) => {
+const fetchFileContent = async (octokit: any, owner: String, repoName: String, filePath: String): Promise<string | null> => {
+  try {
+    const data = await octokit.request(`GET /repos/${owner}/${repoName}/contents/${filePath}`, {
+      owner,
+      repo: repoName,
+      path: filePath,
+      headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    })
+    return Buffer.from(data.data.content as string, 'base64').toString('utf-8');
+  }
+  catch (err) {
+    console.log("Error fetching details for the file:", filePath);
+    return null;
+  }
+}
+
+
+const repoPathGet = async (req: any, res: any) => {
+      const octokit = new Octokit({})
     try {
-        let data = await octokit.request(`GET /repos/Devesh326/contest-list-scrapper/git/blobs/26783bdfdfc8d12b0461cd150c1387d8b1d26023`, {
+        let data = await octokit.request(`GET /repos/Devesh326/contest-list-scrapper/contents/backend/package.json`, {
             owner: 'Devesh326',
             repo: 'contest-list-scrapper',
-            path:'backend',
-            // sha: '26783bdfdfc8d12b0461cd150c1387d8b1d26023',
+            path:'backend/package.json',
             headers: {
                 'X-GitHub-Api-Version': '2022-11-28'
             }
@@ -140,15 +165,81 @@ const repoTreeGet = () => {
     return repoTree;
 }
 
-const repositoryGet = async (req, res) => {
+const repositoryGet = async (req: any, res: any) => {
 
     const folderStructure = getFolderStructure();
     res.json(folderStructure);
 }
 
 const githubWebhookHandler = async (req: any, res: any) => {
-    console.log("Received GitHub webhook:", req.body);
-    res.status(200).send("Webhook received");
+  const data = req.body;
+  console.log("Successfully received the push event");
+  
+    
+    // need to check if the event triggered is a push event and if the branch is main, then only we will trigger the analysis
+    if (data.commits && data.commits.length > 0 && data.ref === 'refs/heads/main') {
+      const repository = data.repository;
+      const repoId = repository.id;
+      const installation_id = data.installation.id;
+      const commits = data.commits;
+      const owner = repository.owner.name
+      const repoName = repository.name;
+      const afterCommitSha = data.after;
+      const ref = data.ref
+
+      await docQueue.add({
+        ref,
+        repoId,
+        owner,
+        repoName,
+        installation_id,
+        commits,
+        repository,
+        afterCommitSha
+      });
+      console.log("added document to the queue");
+      // console.log(req.body);
+      
+    }
+    // res.status(200).send("Webhook received");
 }
 
-export { githubRepoGet, repoTreeGet, repositoryGet, githubRepoTopLevelGet, repoPathGet, githubWebhookHandler };
+
+const createPullReq = async (octokit: any, owner: string, repoName: string, readme: string, ref: string) : any => { 
+  try {
+    // const pr = await octokit.request(`POST /repos/${owner}/${repoName}/pulls`, {
+    //   owner,
+    //   repo: repoName,
+    //   title: 'README.md file generate',
+    //   body: readme,
+    //   head: ref,
+    //   base: ref,
+    //   headers: {
+    //     'X-GitHub-Api-Version': '2022-11-28'
+    //   }
+    // })
+    const pr = await octokit.createPullRequest({
+    owner,
+    repo: repoName,
+    title: "AI Generated README",
+    body: "Generated by AI Documentation Bot",
+    base: "main",
+    head: "ai-doc/readme-update",
+    changes: [
+      {
+        files: {
+          "README.md": readme
+        },
+        commit: "docs: add AI generated README"
+      }
+    ]
+  });
+    return pr;
+  }
+  catch (err){
+    console.error('Failed to create pull request:', err);
+  }
+  return null;
+}
+
+export { githubRepoGet, repoTreeGet, repositoryGet, githubRepoTopLevelGet, repoPathGet, githubWebhookHandler, fetchFileContent, createPullReq };
