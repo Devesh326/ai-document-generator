b8b6ed3707bd3aae28f1e37f24e97915aa2808a7
Devesh326 <agarwaldevesh326@gmail.com>
2026-03-19 20:39:57 +0530

    src/app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import githubRouter from "./routes/githubRoute.js"
 import aiRouter from "./routes/aiRoute.js"
-
+import adminRouter from './routes/adminRoute.js'
+import { getRedisClient } from "./configs/redisConfig.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+// const redisClient = getRedisClient();
 app.use(express.json());
 
 app.use("/github", githubRouter);
@@ -17,6 +18,16 @@ app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
 });
 
+app.use('/admin', adminRouter)
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
+
+process.on("SIGINT", async () => {
+  console.log("Shutting down...");
+
+  await redisClient.quit();
+
+  process.exit(0);
 });
